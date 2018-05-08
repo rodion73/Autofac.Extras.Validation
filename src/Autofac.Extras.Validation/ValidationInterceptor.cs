67c1eca640d8b526066a882e2f5e0caec6366bff
Rodion Novoselov <r.novoselov@outlook.com>
2018-05-08 13:24:36 +0300

    Initial commit.

---
+++ src/Autofac.Extras.Validation/ValidationInterceptor.cs
@@ -0,0 +1,22 @@
+﻿using Castle.DynamicProxy;
+using System.ComponentModel.DataAnnotations;
+
+namespace Autofac.Extras.Validation
+{
+    public class ValidationInterceptor : IInterceptor
+    {
+        #region Public Methods
+
+        public void Intercept(IInvocation invocation)
+        {
+            foreach (var arg in invocation.Arguments)
+            {
+                Validator.ValidateObject(arg, new ValidationContext(arg));
+            }
+
+            invocation.Proceed();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ test/Test.Autofac.Extras.Validation/Test_ValidationInterceptor.cs
@@ -0,0 +1,62 @@
+﻿using Castle.DynamicProxy;
+using FluentAssertions;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Autofac.Extras.Validation
+
+{
+    public class Test_ValidationInterceptor
+    {
+        #region Public Methods
+
+        [Fact]
+        public void Intercept_WithInvalidArg_Throws()
+        {
+            var invocationMock = new Mock<IInvocation>();
+
+            invocationMock.Setup(m => m.Arguments)
+                .Returns(new[] { new Foo() });
+
+            var testee = new ValidationInterceptor();
+
+            testee.Invoking(t => t.Intercept(invocationMock.Object))
+                .Should().Throw<ValidationException>();
+
+            invocationMock.Verify(m => m.Proceed(), Times.Never());
+        }
+
+        [Fact]
+        public void Intercept_WithValidArg_DoesntThrow()
+        {
+            var invocationMock = new Mock<IInvocation>();
+
+            invocationMock.Setup(m => m.Arguments)
+                .Returns(new[] { new Foo { Bar = "bar" } });
+
+            var testee = new ValidationInterceptor();
+
+            testee.Invoking(t => t.Intercept(invocationMock.Object))
+                .Should().NotThrow();
+
+            invocationMock.Verify(m => m.Proceed(), Times.Once());
+        }
+
+        #endregion Public Methods
+
+        #region Private Classes
+
+        private class Foo
+        {
+            #region Public Properties
+
+            [Required]
+            public string Bar { get; set; }
+
+            #endregion Public Properties
+        }
+
+        #endregion Private Classes
+    }
+}
